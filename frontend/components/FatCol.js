89b19050088836afe4a7d61b0748ea7a0367be26
Wenqi He <hewenqi96@gmail.com>
2018-03-05 15:08:37 -0500

    Change JS styles

--- frontend/components/FatCol.js
+++ frontend/components/FatCol.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Grid, Col } from 'react-bootstrap';
 
-const FatCol = ({children}) => (
-    <Grid>
-        <Col md={2}/>
-        <Col md={8}>
-            {children}
-        </Col>
-        <Col md={2}/>
-    </Grid>
+const FatCol = ({ children }) => (
+  <Grid>
+    <Col md={2}/>
+    <Col md={8}>{children}</Col>
+    <Col md={2}/>
+  </Grid>
 );
 
 export default FatCol;
