d67a74ce3d2b8aa12bc6850fed171987919813b5
Wenqi He <hewenqi96@gmail.com>
2018-03-05 15:44:35 -0500

    Change JS styles

--- frontend/containers/Root.prod.js
+++ frontend/containers/Root.prod.js
@@ -1,14 +1,14 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import AppContainer from './AppContainer.js';
 
 export default function Root({ store }) {
-    return (
-        <Provider store={store}>
-            <AppContainer />
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  );
 }
 
 Root.propTypes = {
