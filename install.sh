33bdcff225a97eb51bb612d160c1acb2f779ab21
Wenqi He <hewenqi96@gmail.com>
2019-07-23 15:33:54 -0400

    Fix MySQL server startup issue

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 (cd frontend && npm install && npm run build)
-(cd backend && npm install && mysql -u root < mysql/init.sql)
+(cd backend && npm install &&)
+# Initialize Database
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
+mysql -u root < mysql/init.sql)
