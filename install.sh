7440cfd4c72d50d2759ef3d17d076e90fe53df86
Wenqi He <hewenqi96@gmail.com>
2019-07-24 03:56:08 -0400

    Update build script

--- install.sh
+++ install.sh
@@ -1,11 +1,2 @@
 (cd frontend && npm install && npm run build)
 (cd backend && npm install)
-# Initialize Database
-if [ -d /var/lib/mysql ]; then
-  chown -R mysql /var/lib/mysql
-fi
-if [ -d /var/run/mysqld ]; then
-  chown -R mysql /var/run/mysqld
-fi
-service mysql start
-mysql -u root < mysql/init.sql
